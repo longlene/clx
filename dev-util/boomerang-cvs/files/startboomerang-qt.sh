ef8746dfb7c2446c20a6e200137a81bff154b68b
loong0 <longlene@gmail.com>
2020-07-26 23:27:19 +0800

    cleaup

--- dev-util/boomerang-cvs/files/startboomerang-qt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /usr/local/boomerang
-if [ "$*" = "" ]; then
-	./boomerang-qt
-else
-	./boomerang-qt $*
-fi
--- dev-util/boomerang-cvs/files/startboomerang.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-workdir=`pwd`
-cd /usr/local/boomerang
-if [ "$*" = "" ]; then
-	./boomerang
-else
-	./boomerang -o ${workdir}/output $*
-fi
